f8cc4be085dd281ade4db88cb631afec9d69b6ad
v_wangjie13 <v_wangjie13@baidu.com>
2022-06-20 20:00:07 +0800

    html配置压缩规则

--- build/webpack.conf.js
+++ build/webpack.conf.js
@@ -10,6 +10,7 @@ module.exports = {
     // 指定打包方式为 production || development
     mode: 'development',
     // 指定入口文件 关联js
+    // 按照入口文件的依赖递归查找进行打包
     entry: {
         // 按照入口文件递归查找import相关的依赖进行打包
         'app': './src/index.js'
@@ -23,6 +24,7 @@ module.exports = {
         // 无变化不更新hash，读取之前的缓存文件  变化后更新hash，读取新的文件
         // 不加hash的话，更新文件后，两次访问的地址都是一样的，浏览器就会从缓存读取
         filename: '[name].[hash:8].js'
+        // filename: '[name].[chunkhash:8].js'
     },
     plugins: [
         // 实例化使用
@@ -36,7 +38,20 @@ module.exports = {
             // 因为配置文件在package.json中执行，所以文件路径是根目录下，'./'
             template: 'public/index.html',
             // 用于设置打包输出后的html模板名称
-            filename: 'main.html'
+            filename: 'main.html',
+            // 设置 js 文件是否插入html   css是否插入看后续视频---------
+            inject: true, // 默认为true  false会删除html中的script标签
+            // 配置压缩规则
+            minify: {
+                html5: true, // 根据 HTML5 规范解析输入
+                minifyCSS: true, // 压缩内联css
+                preserveLineBreaks: true, // 当标签之间的空格包含换行符时，总是折叠到 1 个换行符（永远不要完全删除它）
+                collapseWhitespace: true, // 移除空白符和换行符
+                removeComments: true, // 移除html的注释
+                removeAttributeQuotes: true, // 尽可能删除属性周围的引号  
+                removeEmptyAttributes: true, // 删除所有具有纯空格值的属性
+                removeOptionalTags: true // 删除可选标签-- 它只去除 HTML、HEAD、BODY、THEAD、TBODY 和 TFOOT 元素的结束标签。 例如：只有末尾 </head>
+            },
         })
     ],
     // 服务启动配置
