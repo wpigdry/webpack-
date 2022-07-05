2996dafeda85a57a5fe2e8569f88e5b179012e2f
wpigdry <1070683378@qq.com>
2022-07-05 23:45:57 +0800

    配置js内识别import&es6

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,8 @@
         <!-- 打包后，根据这个路径访问的是dist文件夹下的public，因为没有则直接报错 -->
         <!-- 下载copy-webpack-plugin插件复制public的资源到dist打包文件夹下-->
         <img src="public/images/line.png" alt="">
+
+        <button @click="fn">按需加载</button>
     </div>
 </template>
 
@@ -14,11 +16,20 @@
 
 import Img from './assets/wj.png';
 
+// import './assets/index.scss';
+
 export default {
     name: 'App',
     data() {
         return {
             Img
+        }
+    },
+    methods: {
+        // 配置的插件版本错误，点击会报错
+        async fn() {
+            await 2;
+            import('./assets/index.scss')
         }
     }
 }
